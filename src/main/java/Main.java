bf4b4e8d0b13fb88f9827b95b7c0c22e95124899
dionhjx <252417324+dionhjx@users.noreply.github.com>
2026-09-13 22:25:13 +0800

    Implement user input handling and dialog responses

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,7 @@ public class Main extends Application {
     private Button sendButton;
     private Scene scene;
 
+    private Duke duke = new Duke();
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
@@ -32,8 +33,15 @@ public class Main extends Application {
         userInput = new TextField();
         sendButton = new Button("Send");
 
-        DialogBox dialogBox = new DialogBox("Hello!", userImage);
-        dialogContainer.getChildren().addAll(dialogBox);
+
+        //Handling user input
+
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
 
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
@@ -71,6 +79,22 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
 
+
+        //Scroll down to the end every time dialogContainer's height changes.
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+
         //More code to be added here later
     }
+
+    private void handleUserInput() {
+        String userText = userInput.getText();
+        String dukeText = duke.getResponse(userInput.getText());
+
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userText, userImage),
+                DialogBox.getDukeDialog(dukeText, dukeImage)
+        );
+        userInput.clear();
+    }
+
 }
